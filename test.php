ad27da03b7ba26028a96a48c890b65ca1a7e2bc5
manaknight <ryan@manaknightdigital.com>
2019-02-25 09:44:51 -0500

    Update test.php

--- test.php
+++ test.php
@@ -6,3 +6,8 @@ $model = 'abcs';
 $model_result = get_model_name($model);
 echo $model_result['single'];
 echo $model_result['plural'];
+
+execute_complicated_function ([
+  'a' => 1, 
+  'b' => 2
+]);
